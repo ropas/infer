3f10205454af8bc78bd49b6dbd4ac9e17f8a06d6
Sungkeun Cho <skcho@ropas.snu.ac.kr>
2016-11-25 11:17:28 +0900

    Define Itv.plus, Itv.minus

--- infer/src/checkers/itv.ml
+++ infer/src/checkers/itv.ml
@@ -1,12 +1,10 @@
-open BasicDom
-
 module F = Format
 module L = Logging
 
 exception TODO
 
 (* TODO: Due to the side-effect of the symbol numbers, we may have to
-   place the sym_size to the outside of the Itv module. *)
+   place the sym_size outside of the Itv module. *)
 let sym_size = ref 0
 
 module Symbol =
@@ -60,6 +58,33 @@ struct
       let (s1, c1) = M.min_binding x in
       pp1 fmt (s1, c1);
       M.iter (fun s c -> F.fprintf fmt "+%a" pp1 (s, c)) (M.remove s1 x)
+
+  (* Returns (Some n) only when n is not 0. *)
+  let is_non_zero : int -> int option
+  = fun n ->
+    if n = 0 then None else Some n
+
+  let plus : t -> t -> t
+  = fun x y ->
+    let plus' _ n_opt m_opt =
+      match n_opt, m_opt with
+      | None, None -> None
+      | Some v, None
+      | None, Some v -> is_non_zero v
+      | Some n, Some m -> is_non_zero (n + m)
+    in
+    M.merge plus' x y
+
+  let minus : t -> t -> t
+  = fun x y ->
+    let minus' _ n_opt m_opt =
+      match n_opt, m_opt with
+      | None, None -> None
+      | Some v, None -> is_non_zero v
+      | None, Some v -> is_non_zero (-v)
+      | Some n, Some m -> is_non_zero (n - m)
+    in
+    M.merge minus' x y
 end
 
 module Bound =
@@ -133,6 +158,28 @@ struct
     V (n, Coeff.empty)
 
   let initial : t = of_int 0
+
+  let plus : t -> t -> t
+  = fun x y ->
+    match x, y with
+    | MInf, PInf
+    | PInf, MInf -> failwith "+oo + -oo is undefined."
+    | MInf, _
+    | _, MInf -> MInf
+    | PInf, _
+    | _, PInf -> PInf
+    | V (c1, x1), V (c2, x2) -> V (c1 + c2, Coeff.plus x1 x2)
+
+  let minus : t -> t -> t
+  = fun x y ->
+    match x, y with
+    | MInf, MInf
+    | PInf, PInf -> failwith "+oo - +oo and -oo - -oo are undefined."
+    | MInf, _
+    | _, PInf -> MInf
+    | PInf, _
+    | _, MInf -> PInf
+    | V (c1, x1), V (c2, x2) -> V (c1 - c2, Coeff.minus x1 x2)
 end
 
 module ItvPure =
@@ -160,6 +207,18 @@ struct
   let of_int : int -> astate
   = fun n ->
     (Bound.of_int n, Bound.of_int n)
+
+  let top : astate = (Bound.MInf, Bound.PInf)
+
+  let pos : astate = (Bound.of_int 1, Bound.PInf)
+
+  let plus : astate -> astate -> astate
+  = fun (l1, u1) (l2, u2) ->
+    (Bound.plus l1 l2, Bound.plus u1 u2)
+
+  let minus : astate -> astate -> astate
+  = fun (l1, u1) (l2, u2) ->
+    (Bound.minus l1 u2, Bound.minus u1 l2)
 end
 
 include AbstractDomain.BottomLifted(ItvPure)
@@ -168,27 +227,44 @@ type t = astate
 
 let bot = initial
 
-(* TODO *)
-let top = bot
-
-let zero = bot
-
-let one = bot
-
-let pos = bot
-
-let nat = bot
+let top = NonBottom ItvPure.top
 
 let of_int : int -> astate
 = fun n ->
   NonBottom (ItvPure.of_int n)
 
-let le = (<=)
+let zero : astate = of_int 0
 
-let eq x y = raise TODO
+let one : astate = of_int 1
 
-let to_string x = raise TODO
+let pos : astate = NonBottom ItvPure.pos
 
-let plus x y =  raise TODO
+(* TODO *)
+let nat : astate = bot
 
-let minus x y =  raise TODO
+let le : astate -> astate -> bool
+= fun x y ->
+  (<=) ~lhs:x ~rhs:y
+
+let eq : astate -> astate -> bool
+= fun x y ->
+  le x y && le y x
+
+let to_string : astate -> string
+= fun x ->
+  pp F.str_formatter x;
+  F.flush_str_formatter ()
+
+let plus : astate -> astate -> astate
+= fun x y ->
+  match x, y with
+  | Bottom, _
+  | _, Bottom -> Bottom
+  | NonBottom x', NonBottom y' -> NonBottom (ItvPure.plus x' y')
+
+let minus : astate -> astate -> astate
+= fun x y ->
+  match x, y with
+  | Bottom, _
+  | _, Bottom -> Bottom
+  | NonBottom x', NonBottom y' -> NonBottom (ItvPure.minus x' y')
